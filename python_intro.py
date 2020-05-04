a2fff13217430672de190939c971abd0e7b5c43f
connormartinhub <64650662+connormartinhub@users.noreply.github.com>
2020-05-04 16:04:34 -0500

    Add files via upload

---
+++ python_intro.py
@@ -0,0 +1,10 @@
+# Simple input program that asks questions, captures user input and outputs
+import time
+name = input("Hello, what is your name?")
+food = input("What is your favorite food?")
+animal = input("What is your favorite animal?")
+time.sleep(2)
+print("So your name is " + name + ", your favorite food is " + food + ", and your favorite animal is a " + animal + ".")
+time.sleep(4)
+print("My name is Python, I do not eat food, but I like snakes.")
+
